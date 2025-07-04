b2584d9b761dfc0f8d3b19a2491214460eed9555
KadeerUwU <kenhoangkhoaghost@gmail.com>
2025-07-04 09:02:28 +0700

    done order client

--- GenDev_Project/routes/web.php
+++ GenDev_Project/routes/web.php
@@ -23,7 +23,7 @@ use App\Http\Controllers\Client\CartDetailController;
 use Illuminate\Routing\Route as RoutingRoute;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\VerificationController;
-
+use App\Http\Controllers\Client\ClientOrderController;
 
 // Route::get('/products', function () {
 
@@ -150,6 +150,12 @@ Route::prefix('/admin')->middleware(['role:admin|staff'])->group(function () {
 });
 
 Route::resource('/product', ClientProductController::class);
+Route::middleware(['auth', 'verified'])->prefix('orders')->name('client.orders.')->group(function () {
+    Route::get('/', [ClientOrderController::class, 'index'])->name('index');
+    Route::get('/{order}', [ClientOrderController::class, 'show'])->name('show');
+    Route::put('/{order}/cancel', [ClientOrderController::class, 'cancel'])->name('cancel');
+});
+
 
 // ================= TÀI KHOẢN =================
 
