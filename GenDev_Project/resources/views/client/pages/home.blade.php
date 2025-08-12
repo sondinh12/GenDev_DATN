101c90019c8ecfb6c212809a52d412a69dcbae71
VietHoangPH49147 <nguyenviethoang28032005@gmail.com>
2025-08-12 16:20:11 +0700

    up

--- GenDev_Project/resources/views/client/pages/home.blade.php
+++ GenDev_Project/resources/views/client/pages/home.blade.php
@@ -172,7 +172,7 @@
                                     </div>
                                 </div>
                             </div>
-
+                        </div>
                     </main>
                 </div>
             </div>
