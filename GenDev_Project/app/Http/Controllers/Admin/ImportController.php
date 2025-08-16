f59093986fca8ebd48461ba507ed67f91daa2f1f
sondinh12 <nguyendinhson92005@gmail.com>
2025-08-16 21:28:35 +0700

    thêm tìm kiếm hóa đơn và nhà cung cấp

--- GenDev_Project/app/Http/Controllers/Admin/ImportController.php
+++ GenDev_Project/app/Http/Controllers/Admin/ImportController.php
@@ -23,9 +23,22 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class ImportController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $imports = Import::with('supplier')->orderBy('created_at', 'desc')->paginate(10);
+        // $imports = Import::with('supplier')->orderBy('created_at', 'desc')->paginate(10);
+        $query = Import::with('supplier')->orderBy('created_at', 'desc');
+
+        // Nếu có từ khóa tìm kiếm
+        if ($request->filled('keyword')) {
+            $keyword = $request->input('keyword');
+
+            // Tìm theo tên supplier
+            $query->whereHas('supplier', function ($q) use ($keyword) {
+                $q->where('name', 'like', '%' . $keyword . '%');
+            });
+        }
+
+    $imports = $query->paginate(10);
         return view('Admin.imports.index', compact('imports'));
     }
 
